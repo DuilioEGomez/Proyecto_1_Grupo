19a1f83d48b07486391d02c28d53d672e1542ffc
Ignacio Gálvez <igalvez_13@hotmail.com>
2023-04-30 17:23:48 -0300

    cambio 2

--- Proyecto_1_Grupo/Program.cs
+++ Proyecto_1_Grupo/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Proyecto_1_Grupo
+﻿using System;
+
+namespace Proyecto_1_Grupo
 {
     internal class Program
     {
+        struct Tesla
+        {
+            public string modelo;
+            public int anio;
+            public int kmActual;
+            public int kmService;
+            public string color;
+            public string duenio;
+        }
         static void Main(string[] args)
         {
-
+            Array teslas = new Tesla;  
         }
     }
 }
