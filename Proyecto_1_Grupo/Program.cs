16b8533b0883dd80e42f16d722bdb0c5419fd745
DuilioEGomez <duiliohenry@gmail.com>
2023-05-06 14:02:42 -0300

    ooojj

--- Proyecto_1_Grupo/Program.cs
+++ Proyecto_1_Grupo/Program.cs
@@ -12,8 +12,8 @@ namespace Proyecto_1_Grupo
         {
             public string modelo;
             public int anio;
-            public long kmActual;
-            public long kmService;
+            public int kmActual;
+            public int kmService;
             public string color;
             public string duenio;
         }
